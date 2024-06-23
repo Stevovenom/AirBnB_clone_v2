cbd14e67a634a9b84d5a27eb1095056517e700e6
stevovenom <stephenokoth2002@gmail.com>
2024-06-23 12:32:00 -0700

    update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'state'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('city', backref='state', cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initialize state"""
+        super().__init__(*args, **kwargs)
