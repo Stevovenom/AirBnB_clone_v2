2a0d324c5a12547b04a5d8e67b20927f1c866dd0
stevovenom <stephenokoth2002@gmail.com>
2024-06-27 01:41:46 -0700

    updated the place model

--- models/place.py
+++ models/place.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-import os  # Add this line to import the os module
-
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+import os
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+
+# Define the place_amenity table for the Many-to-Many relationship
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -22,4 +27,28 @@ class Place(BaseModel, Base):
     latitude = Column(Float) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
     longitude = Column(Float) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else 0.0
     amenity_ids = [] if os.getenv('HBNB_TYPE_STORAGE') != 'db' else None
+
     user = relationship('User', back_populates='places') if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    reviews = relationship('Review', back_populates='place', cascade='all, delete-orphan') if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    amenities = relationship('Amenity', secondary=place_amenity, viewonly=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            """Returns the list of Review instances with place_id equals to the current Place.id"""
+            from models import storage
+            from models.review import Review
+            return [review for review in storage.all(Review).values() if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances based on the attribute amenity_ids"""
+            from models import storage
+            from models.amenity import Amenity
+            return [storage.all(Amenity).get(amenity_id) for amenity_id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter for amenities that handles append method for adding an Amenity.id"""
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
