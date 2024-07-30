f03eb66027652784e427e41755bbfe9f52de973a
stevovenom <stephenokoth2002@gmail.com>
2024-07-30 05:52:35 -0700

    task 3 task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Route to display 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Route to display 'HBNB' """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Route to display 'C ' followed by the value of the text variable """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """ Route to display 'Python ' followed with value variable value"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
