53c78df8d08aba04f88c1b5630fe8e81aadb04dd
stevovenom <stephenokoth2002@gmail.com>
2024-07-31 06:14:36 -0700

    trial fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-""" 7. Start flask service that does something. """
+""" 7. Start Flask service that does something. """
 
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
+from models import storage
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-#@app.route()
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the SQLAlchemy session."""
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """Display a list of states."""
+    states = storage.all("State")  # Use storage to fetch data
+    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == '__main__':
