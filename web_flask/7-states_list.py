9e212a40e583eea1eb272ddfee8f1098a86705c2
stevovenom <stephenokoth2002@gmail.com>
2024-07-31 05:57:01 -0700

    fixed module documentation

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 # web_flask/7-states_list.py
 
+"""
+7-states_list.py
+
+This module sets up a Flask web application that provides an endpoint to
+display a list of states. The states are retrieved from a storage system
+and presented on a web page.
+
+Modules:
+- Flask: Web framework used to create the application.
+- render_template: Function to render HTML templates.
+- storage: Custom storage object for database interactions.
+"""
+
 from flask import Flask, render_template
 from models import storage
 
@@ -9,13 +22,23 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the SQLAlchemy session."""
+    """
+    Close the SQLAlchemy session after each request.
+
+    Args:
+        exception (Exception): The exception that occurred during request processing.
+    """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a list of states."""
+    """
+    Display a list of states sorted by name.
+
+    Returns:
+        str: Rendered HTML template displaying the list of states.
+    """
     states = storage.all("State")
     sorted_states = sorted(states.values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
