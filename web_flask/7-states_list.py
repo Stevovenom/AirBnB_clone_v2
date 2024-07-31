3cab6cbb5e893b8d21ee35412bd70b74d89c7b15
stevovenom <stephenokoth2002@gmail.com>
2024-07-31 07:13:33 -0700

    final trial debug with still check 6, 7 and 9 failing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,21 +2,21 @@
 """
 Starts a Flask web application that displays a list of states.
 
-This script sets up a Flask web application with a single route, `/states_list`,
+The script sets a Flask web application with a single route, `/states_list`,
 that displays a list of states retrieved from the `storage` module. The states
 are sorted by name before being passed to the template for rendering.
 
 Modules:
-- flask: Provides the Flask framework for creating the web application and rendering templates.
+- flask: Provides necessities for creating application and rendering templates
 - models: Imports the necessary modules from the `models` package.
 - models.storage: Handles data storage and retrieval operations.
 
 Functions:
-- states_list: Handles the `/states_list` route to fetch, sort, and display states.
-- teardown_db: Closes the SQLAlchemy session after each request to ensure proper resource management.
+- states_list: Handles the `/states_list` route to fetch and  display states
+- teardown_db: Closes SQLAlchemy session after request for resouce mngt
 
 Routes:
-- `/states_list`: Displays an HTML page listing all State objects in alphabetical order. The states are rendered using the `7-states_list.html` template.
+- `/states_list`: Displays an HTML page listing all State in alphabetical order
 
 Application Configuration:
 - Host: `0.0.0.0`
@@ -33,13 +33,14 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
     Displays a list of states.
 
     Fetches all states from the storage, sorts them by name, and passes them
-    to the `7-states_list.html` template for rendering. The states are displayed
+    to the `7-states_list.html` for rendering. The states are displayed
     in an HTML format within an unordered list.
 
     Returns:
@@ -47,6 +48,7 @@ def states_list():
     """
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
@@ -61,6 +63,7 @@ def teardown_db(exception):
         exception (Exception): The exception raised during the request, if any.
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
